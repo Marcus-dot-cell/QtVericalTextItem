f22fd591699f524c37b232ea2ae645fecb3eeff1
fisher <feijunwu@icloud.com>
2022-05-20 14:56:07 +0800

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -1,20 +1,164 @@
 #include "widget.h"
-#include <QTextEdit>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include "cgraphicsedit.h"
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QFontComboBox>
+#include <QCheckBox>
+#include <QLabel>
+#include <QColorDialog>
+#include <QFile>
+
 Widget::Widget(QWidget *parent)
-    : QWidget{parent}
+    : QWidget(parent)
 {
-    QTextEdit* txtScrollEdit = new QTextEdit(this);
-    txtScrollEdit->setFixedSize(this->size());
-    connect(txtScrollEdit, SIGNAL(textChanged()), this, SLOT(slotTextChanged()));
+    setWindowState(Qt::WindowMaximized);
+
+    fontComboBox = new QFontComboBox();
+    fontComboBox->setEditable(false);
+
+    colorBtn = new QPushButton(tr("color select"));
+    slider = new QSlider();
+    slider->setMinimum(1);
+    slider->setMaximum(1000);
+    slider->setOrientation(Qt::Horizontal);
+    slider->setFixedWidth(300);
+
+    boldCheckBox = new QCheckBox(tr("Bold"));
+    italicCheckBox = new QCheckBox(tr("Italic"));
+    delLineCheckBox = new QCheckBox(tr("Overline"));
+    underlineCheckBox = new QCheckBox(tr("Underline"));
+    strikeOutCheckBox = new QCheckBox(tr("StrikeOut"));
+    aligentComboBox = new QComboBox();
+    aligentComboBox->addItems({"Top", "Center", "Bottom"});
+    rowspacingComboBox = new QComboBox();
+    rowspacingComboBox->addItems({"0", "6", "8", "9", "10", "20", "30", "40", "50", "60", "70", "80"});
+    letterspacingComboBox = new QComboBox;
+    letterspacingComboBox->addItems({"200", "100", "75","50", "25", "10", "0", "-10", "-25", "-50", "-75", "-100"});
+
+    QGraphicsScene* scene = new QGraphicsScene();
+    view = new QGraphicsView();
+
+    textEdit = new CGraphicsEdit();
+    scene->addItem(textEdit);
+
+    view->setScene(scene);
+
+    QHBoxLayout* hLayout = new QHBoxLayout;
+    hLayout->addWidget(new QLabel(tr("TextSize: ")));
+    hLayout->addWidget(slider);
+    hLayout->addStretch();
+
+    QHBoxLayout* hLayout2 = new QHBoxLayout;
+    QPushButton* saveBtn = new QPushButton(tr("Save"));
+    QPushButton* loadBtn = new QPushButton(tr("Load"));
+    hLayout2->addWidget(saveBtn);
+    hLayout2->addWidget(loadBtn);
+
+    QVBoxLayout* vLayout = new QVBoxLayout;
+    vLayout->addWidget(fontComboBox);
+    vLayout->addWidget(boldCheckBox);
+    vLayout->addWidget(italicCheckBox);
+    vLayout->addWidget(delLineCheckBox);
+    vLayout->addWidget(underlineCheckBox);
+    vLayout->addWidget(strikeOutCheckBox);
+    vLayout->addWidget(colorBtn);
+    vLayout->addLayout(hLayout);
+    vLayout->addWidget(aligentComboBox);
+    vLayout->addWidget(rowspacingComboBox);
+    vLayout->addWidget(letterspacingComboBox);
+    vLayout->addLayout(hLayout2);
+    vLayout->addStretch();
+
+    QHBoxLayout* mainLayout = new QHBoxLayout();
+    mainLayout->addLayout(vLayout, 1);
+    mainLayout->addWidget(view, 5);
+    this->setLayout(mainLayout);
+
+    connect(fontComboBox, &QFontComboBox::currentTextChanged, textEdit, &CGraphicsEdit::onFontChanged);
+    connect(colorBtn, &QPushButton::clicked, this, &Widget::onSelectColor);
+    connect(saveBtn, &QPushButton::clicked, this, &Widget::onSave);
+    connect(loadBtn, &QPushButton::clicked, this, &Widget::onLoad);
+    connect(boldCheckBox, &QCheckBox::clicked, this, &Widget::onCheckBoxClicked);
+    connect(italicCheckBox, &QCheckBox::clicked, this, &Widget::onCheckBoxClicked);
+    connect(delLineCheckBox, &QCheckBox::clicked, this, &Widget::onCheckBoxClicked);
+    connect(underlineCheckBox, &QCheckBox::clicked, this, &Widget::onCheckBoxClicked);
+    connect(strikeOutCheckBox, &QCheckBox::clicked, this, &Widget::onCheckBoxClicked);
+    connect(slider, &QSlider::valueChanged, this, &Widget::onFontSizeChanged);
+    connect(rowspacingComboBox, &QComboBox::currentTextChanged, this, &Widget::onRowSpaceChanged);
+    connect(aligentComboBox, &QComboBox::currentTextChanged, this, &Widget::onaligentchanged);
+    connect(letterspacingComboBox, &QComboBox::currentTextChanged, this, &Widget::onLetterSpaceChanged);
 }
 
-void Widget::slotTextChanged()
+void Widget::onSelectColor()
 {
-    QTextEdit* txtScrollEdit = (QTextEdit*)sender();
-    QStringList sl;
-    sl=txtScrollEdit->toPlainText().split(" ");
-    txtScrollEdit->setText(sl.join("\n\n"));
-    txtScrollEdit->setWordWrapMode (QTextOption::WrapAnywhere);// txtScrollEdit is QTextEdit[/COLOR]
-    txtScrollEdit->setLineWrapMode(QTextEdit::FixedColumnWidth);
-    txtScrollEdit->setLineWrapColumnOrWidth(1);
+    QColorDialog dialog(this);
+    QObject::connect(&dialog, &QColorDialog::currentColorChanged, textEdit, &CGraphicsEdit::onColorSelected);
+    dialog.exec();
 }
+
+void Widget::onSave()
+{
+    QString htmlText = textEdit->toHtml();
+    //save
+    if (!htmlText.isEmpty()) {
+        QFile f("./test.html");
+        if (f.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            f.write(htmlText.toLocal8Bit());
+            f.close();
+        }
+    }
+}
+
+void Widget::onLoad()
+{
+    QString htmlStr;
+    QFile f("./test.html");
+    if (f.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QByteArray data = f.readAll();
+        htmlStr = QString::fromLocal8Bit(data);
+    }
+    textEdit->setText(htmlStr);
+}
+
+void Widget::onCheckBoxClicked(bool checked)
+{
+    QCheckBox* box = (QCheckBox*)sender();
+    if (box == boldCheckBox) {
+        textEdit->setBold(checked);
+    } else if (box == italicCheckBox) {
+        textEdit->setItalic(checked);
+    } else if (box == delLineCheckBox) {
+        textEdit->setOverline(checked);
+    } else if (box == underlineCheckBox) {
+        textEdit->setUnderline(checked);
+    } else if (box ==strikeOutCheckBox) {
+        textEdit->setStrikeOut(checked);
+    }
+}
+
+void Widget::onFontSizeChanged(int value)
+{
+    textEdit->setFontSize(value);
+}
+
+void Widget::onRowSpaceChanged(const QString& text)
+{
+    int spacing = text.toInt();
+    textEdit->setColumnSpacing(spacing);
+}
+
+void Widget::onaligentchanged(const QString& text)
+{
+    int index = text == "Top" ? 0 : text == "Center" ? 1 : 2;
+    textEdit->setAlignment((CGraphicsEdit::TextDirection)index);
+}
+
+void Widget::onLetterSpaceChanged(const QString& text)
+{
+    int spacing = text.toInt();
+    textEdit->setLetterSpacing(spacing);
+}
+
